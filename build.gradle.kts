1e87c05bd3b252f72a92acfcbc9b5a859b6398ea
Rohit Kumar <rollychop@gmail.com>
2025-07-05 08:07:39 +0530

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "text-typer"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
         }
     }
 }
